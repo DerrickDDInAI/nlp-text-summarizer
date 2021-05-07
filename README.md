a9948997d969af2e45d4b30d6b4dffedc908512e
Derrick DD Van Frausum <derrick.vanfrausum@gmail.com>
2021-05-07 09:31:47 +0200

    delete core directory; summarizer

--- README.md
+++ README.md
@@ -42,9 +42,6 @@ To run the program and see a demo of the code, you need:
 | ---------------- | :----------------------------------------------|
 | Numpy            | To handle Numpy arrays                         |
 | Pandas           | To store and access info in a DataFrame        |
-| Matplotlib       | To plot the data                               |
-| Scikit-learn     | To preprocess dataset, perform machine learning|
-| jupyter          | To see Exploratory Data Analysis and live demo |
 
 
 Follow these instructions to install the required libraries: on terminal
@@ -58,29 +55,16 @@ Follow these instructions to install the required libraries: on terminal
 Note that I develop the source code on macOS Big Sur
 
 ## Data Sources
-To train my machine learning model, I use the datasets from
-- *Kepler*;
-- *K2*;
-- *TESS*.
 
-You can download the public datasets here: 
-
-Source:
-...
 
 ## Instructions
 ### How to run the program
-- Run `main.py` to start the program.
-- Run `main_demo.ipynb` to see a live demo of the program.
-Or
 On your terminal:
 1. Open your terminal;
-2. cd to the directory where the `main.py` or `main_demo.ipynb` are
+2. cd to the directory
 3. Activate your virtual environment.
 4. Run the command:
-- `python3 main.py` (to run the program)
-- `jupyter notebook main_demo.ipynb` (to open the jupyter notebook)
-- `flask run` and browse to the link provided (to run the app locally in your browser)
+`flask run` and browse to the link provided (to run the app locally in your browser)
 
 
 ### Usage example
@@ -92,43 +76,17 @@ On your terminal:
 The project is structured as follows:
 
 ```
-astro
+nlp-text-summarizer
 │   README.md               :explains the project
 │   main.py                 :script to run in order to start the program
-│   main_demo.ipynb         :jupyter notebook to see a demo of the program
 │   requirements.txt        :packages to install to run the program
 │   .gitignore              :files to ignore when pushing to the GitHub repository
 │
-└───core                    :directory contains all the core scripts of the program
-│   │   __init__.py
-│   │
-│   └───assets              :contains the datasets, images and ML models
-│       ├───data
-│       ├───images
-│       └───models
-│   └───tutorials           :contains the tutorials I follow (mainly from lightkurve)
+└───app                    :directory contains all the core scripts of the app
 ```
 
 ### Roadmap
-- [x] Understand the ways to find planets
-- [ ] Follow lightkurve tutorials
-- [ ] Download the dataset
-- [ ] Exploratory Data Analysis
-- [ ] Clean dataset
-- [ ] Prepare dataset for machine learning
-- [ ] If labeled data, build ML models for predictive classification
-- [ ] If no labeled data, build ML models for clustering
-- [ ] Select the right performance metrics for your model
-- [ ] Evaluate the distribution of datapoints and evaluate its influence in the model
-- [ ] Tuning parameters of the model for better performance
-- [ ] Identify if models underfit or overfit and use techniques to prevent it
-- [ ] Select best model according to performance metrics
-- [ ] Define the strengths and limitations of the model
 
-Finally:
-- [ ] Draw conclusions from data analysis and predictions
-- [ ] Optimize code
-- [ ] Prepare tech talk presentation (10 minutes + 5 minutes Q&A)
 
 
 ### Author(s) and acknowledgment
--- app/summarizer.py
+++ app/summarizer.py
@@ -8,64 +8,111 @@ Python script to summarize small and large text
 # import internal modules
 import re
 import requests
-import time
 from typing import List, Set, Dict, TypedDict, Tuple, Optional
 
 # import 3rd-party modules
-import pandas as pd
-# from transformers import pipeline
-# import gensim
-from gensim.summarization import summarize
+from bs4 import BeautifulSoup as bs
+from gensim.summarization import summarize as gensim_summarize
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+
 
 # import local modules
+from decorators import timer
 
 # load pre-trained summarization pipeline
 # global summarizer
 # summarizer = pipeline("summarization")
 
 # =====================================================================
+# Functions
+# =====================================================================
+
+# decorator to measure runtime of function
+@timer
+def gensim_summary(text, ratio=0.2, word_count=None, split=False):
+    """
+    Function to perform extractive text summarization
+    with Gensim
+    * ratio = proportion of summary compared with text
+    * word_count = number of words in summary
+    """
+    return gensim_summarize(text, ratio, word_count, split)
+
+
+# =====================================================================
 # Classes
 # =====================================================================
 
-class PipelineSummarizer():
+class TransformersPipeline():
     """
-    PipelineSummarizer
-    It has 2 attributes: name, xxx
-    * name: xxx
-    * xxx: xxx
-    Class attributes:
-    * xxx:
-    * xxx:
+    TransformersPipeline class is a wrapper for HuggingFace transformers
     """
-
-    # class attributes
-
-    def __init__(
-        self,
-        xy_position: tuple,
-        size=50,
-        mass: int = 1,
-        name: str = None,
-        color: tuple = (0, 0, 255),  # RGB color code for blue
-    ) -> None:
+    def __init__(self, model_identifier: str, pipeline) -> None:
         """
         Function to create an instance of PipelineSummarizer class
-        By default:
-        * xxx if no name is provided
-        * xxx
+        model_identifier = string identifier of model:
+        e.g. "facebook/bart-large-cnn"
         """
-        pass
+        self.tokenizer = AutoTokenizer.from_pretrained(model_identifier)
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(model_identifier)
+        self.summarizer = pipeline("summarization", model=self.model, tokenizer=self.tokenizer)
 
 
-def pipeline_summarize(text, summarizer, min_length=30, max_length=130):
-    """
-    Function to summarize a text
-    """
-    # summarize text
-    summary = summarizer(text, min_length=min_length, max_length=max_length, do_sample=False)
+    def chunk_text(self, text: str) -> List[str]:
+        """
+        Function to chunk long text
+        according to tokenizer.model_max_length
+        """
+        # Get max number of inputs possible for model
+        max_chunk_len = self.tokenizer.model_max_length
 
-    return summary[0]["summary_text"]
+        # Replace [.?!] by [.?!]<EOS> 
+        pattern = re.compile(r'(?P<punctuation>[.?!])')
+        text = pattern.sub('\g<punctuation><EOS>', text)
 
+        # split in sentences
+        sentences: str = text.split('<EOS>')
+
+        # create list and current_chunk counter
+        chunks: List[str] = []
+        current_chunk: int = 0
+        
+        for sentence in sentences:
+            if len(chunks) == current_chunk + 1:
+
+                # if adding a sentence doesn't exceed max_chunk_len 
+                if len(chunks[current_chunk]) + len(sentence.split(' ')) <= max_chunk_len:
+
+                    # add tokens to current chunk
+                    chunks[current_chunk].extend(sentence.split(' '))
+                
+                # else add tokens to next chunk
+                else:
+                    current_chunk += 1
+                    chunks.append(sentence.split(' '))
+            
+            # add tokens to 1st chunk
+            else:
+                chunks.append(sentence.split(' '))
+
+            # for each chunk, join tokens to make a single string
+            for i in range(len(chunks)):
+                chunks[i] = ' '.join(chunks[i])
+            
+            return chunks
+
+    # decorator to measure runtime of function
+    @timer
+    def summarize(self, text, min_length=30, max_length=200):
+        """
+        Function to summarize a text 
+        or list of text(to deal with long documents)
+        """
+        # summarize text
+        summaries = self.summarizer(text, min_length=min_length, max_length=max_length, do_sample=False)
+        summary = ' '.join([summary['summary_text'] for summary in summaries])
+        
+        return summary
 
 # =====================================================================
 # Main Function
@@ -84,22 +131,20 @@ def main():
     text = [result.text for result in results]
     book = ' '.join(text)
 
+    # Extractive summary
+    extractive_summary = gensim_summary(book)
 
-    # With gensim
+    # Instantiate TransformersPipeline class
+    facebook_bart_large_cnn = TransformersPipeline("facebook/bart-large-cnn", pipeline)
+    extractive_summary_chunks = facebook_bart_large_cnn.chunk_text(extractive_summary)
 
-    # start timer
-    start_time = time.time()
-
-    # pass text corpus to summarizer
-    summary = summarize(book_text)
-
-    # stop timer and compute the execution time
-    end_time = time.time()
-    diff_time = (end_time - start_time)
-    print(f"\nTime to summarize: {diff_time:.2f} seconds")
+    summary = facebook_bart_large_cnn.summarize(extractive_summary_chunks)
     
-    # export to txt file
-    with open("extractive_summary_around_the_world.txt", "w") as file:
+    # export to txt files
+    with open("static/data/extractive_summary_gensim.txt", "w") as file:
+        file.write(extractive_summary)
+    
+    with open("static/data/summary_facebook_bart.txt", "w") as file:
         file.write(summary)
 
 # =====================================================================
